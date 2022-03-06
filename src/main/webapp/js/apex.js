5166041d93538c71ea2e662c6d5cee72da122c68
maw <malikiw20@gmail.com>
2022-03-06 11:05:13 -0500

    api link

--- src/main/webapp/js/apex.js
+++ src/main/webapp/js/apex.js
@@ -1,6 +1,7 @@
-function httpGet("https://public-api.tracker.gg/v2/apex/standard/profile/psn/L0ewki_") {
+var link = "https://public-api.tracker.gg/v2/apex/standard/profile/psn/L0ewki_";
+function httpGet(link) {
   let xmlHttpReq = new XMLHttpRequest();
-  xmlHttpReq.open("GET", "https://public-api.tracker.gg/v2/apex/standard/profile/psn/L0ewki_", false); 
+  xmlHttpReq.open("GET", link, false); 
   xmlHttpReq.send(null);
 
   return xmlHttpReq.responseText;
